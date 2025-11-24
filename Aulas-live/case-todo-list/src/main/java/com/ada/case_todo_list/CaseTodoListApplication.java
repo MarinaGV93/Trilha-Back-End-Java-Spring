c1d05fa4d76a4b22d6e4793a6e0a7e6fb78abb29
Marina  Golao Vale <56769864+MarinaGV93@users.noreply.github.com>
2025-11-24 19:48:25 -0300

    Add files via upload

---
+++ Aulas-live/case-todo-list/src/main/java/com/ada/case_todo_list/CaseTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.ada.case_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseTodoListApplication.class, args);
+	}
+
+}
---
+++ Aulas-live/case-todo-list/src/test/java/com/ada/case_todo_list/CaseTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.case_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
